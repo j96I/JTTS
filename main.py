3f3cd22e2e9ef89730cf71a97366794cdf5e93b7
j96I <j.ibrahim@me.com>
2025-04-15 22:00:06 +1000

    close

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import torch
 from TTS.api import TTS
 
-device = torch.cuda.is_available()
+# def download_model():
+#     # Print available TTS models
+#     tts_manager = TTS().list_models()
+#     all_models = tts_manager.list_models()
+#     print("TTS models:\n", all_models, "\n", sep="")
 
-# Check if CUDA is installed
-if device:
-    print("CUDA installed successfully\n") 
-else:
-    print("CUDA not properly installed. Stopping process...")
-    quit()
+#     # Prompt model selection
+#     model = input("Enter model:\n")
+#     # for example, tts_models/multilingual/multi-dataset/xtts_v2
 
-# Print available TTS models
-view_models = input("View models? [y/n]\n")
-if view_models == "y":
-    tts_manager = TTS().list_models()
-    all_models = tts_manager.list_models()
-    print("TTS models:\n", all_models, "\n", sep = "")
 
-# Prompt model selection
-model = input("Enter model:\n")
-# for example, tts_models/multilingual/multi-dataset/xtts_v2
+def test_model(model_path, config_path):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Example voice cloning with selected model
-tts = TTS((model), progress_bar=True).to(device)
-tts.tts_to_file("This is a voice cloning test", speaker_wav="train-audio.wav", language="en", file_path="output.wav")
+    # Initialize TTS with your model files
+    tts = TTS(model_path=model_path, config_path=config_path, progress_bar=True).to(
+        device
+    )
+
+    tts.tts_to_file(
+        "This is a voice cloning test",
+        speaker_wav="train-audio.wav",
+        language="en",
+        file_path="output.wav",
+    )
+
+    tts.tts_to_file(
+        text="Hello world!",
+        speaker="train-audio.wav",
+        language="english",
+        file_path="output.wav",
+    )
+
+
+model_path = r"C:\Users\User\Documents\Coqui\TTS\res\model_file.pth"
+config_path = r"C:\Users\User\Documents\Coqui\TTS\res\config.json"
+# model_name = "tts_models/en/blizzard2013/capacitron-t2-c50"
+
+test_model(model_path, config_path)
